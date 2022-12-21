ae45297336cb91b07e608663810278c1109d495e
Bruno Lima <bruno-amil@hotmail.com>
2022-12-20 22:01:05 -0300

    feat: configurando o react query e mirageJS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,27 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import { theme } from "../styles/theme";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
+import { makeServer } from "../services/mirage";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={theme}>
       <SidebarDrawerProvider>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </SidebarDrawerProvider>
     </ChakraProvider>
+    <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
